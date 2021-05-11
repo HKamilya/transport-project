cb9d0159ebe2ca46df0895004433d8d00ed95098
HKamilya <33655453+HKamilya@users.noreply.github.com>
2021-05-11 23:29:39 +0300

    Update Logging.java

--- transport-project-impl/src/main/java/ru/kpfu/itis/transportprojectimpl/aspect/Logging.java
+++ transport-project-impl/src/main/java/ru/kpfu/itis/transportprojectimpl/aspect/Logging.java
@@ -4,13 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 
 @Aspect
 @Component
-@Slf4j
 public class Logging {
+    private static final Logger log = LoggerFactory.getLogger(Logging.class);
 
     @Pointcut("execution(public * ru.kpfu.itis.transportprojectimpl.service.*.*(..))")
     public void stringProcessingMethods() {
@@ -28,7 +30,6 @@ public class Logging {
         long start = System.currentTimeMillis();
         Object proceed = joinPoint.proceed();
         long executionTime = System.currentTimeMillis() - start;
-        System.out.println("hi");
         log.info(joinPoint.getSignature() + " выполнен за " + executionTime + "мс");
         return proceed;
     }
